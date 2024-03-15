43358965e8d15064d0653c448784325660c474fb
Newgatw <newtonbussmeyer@gmail.com>
2024-03-14 22:47:51 -0300

    bancoDeDados

---
+++ PROJETOS LOG PROG/Lista04/L04E02/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Newton";
+        float nota = 8.5f;
+        System.out.format("A nota de %s é: %.1f", nome, nota);
+
+    }
+}
